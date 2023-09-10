c656b4b7a1d86084029b97ee6e0d7ca80dd54a41
Alexander Weimer <i@alexander.systems>
2023-09-10 15:13:01 -0500

    Update run.js

--- run.js
+++ run.js
@@ -1,1 +1,24 @@
 
+// Take screenshot of element using HTML2canvas library
+function takeShot() {
+  // Enable display of background text elements
+  document.getElementById('line1Copy').style.visibility = "visible";
+  document.getElementById('line2Copy').style.visibility = "visible";
+  document.getElementById('line3Copy').style.visibility = "visible";
+  // Clear old output if present
+  document.getElementById('output').innerHTML = "";
+let div =
+    document.getElementById('signboard');
+
+// Take screenshot
+html2canvas(div,{backgroundColor:null}).then(
+    function (canvas) {
+        document
+        .getElementById('output')
+        .appendChild(canvas);
+    })
+// Rehide background text elements
+document.getElementById('line1Copy').style.visibility = "hidden";
+document.getElementById('line2Copy').style.visibility = "hidden";
+document.getElementById('line3Copy').style.visibility = "hidden";
+}
