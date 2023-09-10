6e8453f4a944a404e7ca87ad9e49d93775c9d6b3
Alexander Weimer <i@alexander.systems>
2023-09-10 15:13:12 -0500

    Update run.js

--- run.js
+++ run.js
@@ -1,3 +1,16 @@
+// Copy over text from input to background element
+function sync() {
+    console.log('Attempting Sync')
+    var line1 = document.getElementById('line1'),
+        line1Copy = document.getElementById('line1Copy'),
+        line2 = document.getElementById('line2'),
+        line2Copy = document.getElementById('line2Copy'),
+        line3 = document.getElementById('line3'),
+        line3Copy = document.getElementById('line3Copy');
+    line1Copy.innerHTML = line1.value;
+    line2Copy.innerHTML = line2.value;
+    line3Copy.innerHTML = line3.value;
+}
 
 // Take screenshot of element using HTML2canvas library
 function takeShot() {
